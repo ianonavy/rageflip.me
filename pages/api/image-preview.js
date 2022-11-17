5b911ac56842d258827a8c37f1e3d8f8f8537750
Ian Adam Naval <790279+ianonavy@users.noreply.github.com>
2022-11-17 02:00:11 -0500

    Log text for debugging character encoding issue

--- pages/api/image-preview.js
+++ pages/api/image-preview.js
@@ -1,6 +1,7 @@
 import { getRageImage } from "../../lib/image";
 
 export default function handler(req, res) {
+  console.log("handler", req.query.text);
   const image = getRageImage(req.query.text);
 
   res.setHeader("Content-Type", "image/png");
