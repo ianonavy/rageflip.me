85dad8feb901288f14b47e575b8b5ea831850b92
Ian Adam Naval <790279+ianonavy@users.noreply.github.com>
2022-11-17 01:48:36 -0500

    Set Content-Type header

--- pages/api/image-preview.js
+++ pages/api/image-preview.js
@@ -2,5 +2,8 @@ import { getRageImage } from "../../lib/image";
 
 export default function handler(req, res) {
   const image = getRageImage(req.query.text);
-  res.status(200).write(image);
+
+  res.setHeader("Content-Type", "image/png");
+  res.setHeader("Content-Disposition", "inline");
+  res.send(image);
 }
