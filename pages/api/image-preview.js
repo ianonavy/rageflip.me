3d3e5a261e21041ef09adf287008ba13c9136c01
Ian Adam Naval <790279+ianonavy@users.noreply.github.com>
2022-11-17 01:42:31 -0500

    Switch to non-cairo canvas

--- pages/api/image-preview.js
+++ pages/api/image-preview.js
@@ -1,6 +1,6 @@
 import { getRageImage } from "../../lib/image";
 
 export default function handler(req, res) {
-  const stream = getRageImage(req.query.text);
-  stream.pipe(res.status(200));
+  const image = getRageImage(req.query.text);
+  res.status(200).write(image);
 }
