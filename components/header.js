523fd0608875f054dd08cc980b671fc75f46373c
Ian Adam Naval <790279+ianonavy@users.noreply.github.com>
2022-11-19 17:00:37 -0500

    Fix missing og:image

--- components/header.js
+++ components/header.js
@@ -10,6 +10,7 @@ export default function Layout({ description, og }) {
         name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0"
       />
+      {og ? <meta property="og:image" content={og} /> : ""}
       <link rel="icon" href="/favicon.ico" />
     </Head>
   );
